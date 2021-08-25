1a8e6fdff704d26ef8079e83d67e5a89fe014957
OiEuSouOPi <rafasc866@gmail.com>
2021-08-25 17:41:11 -0300

    arranjos JS

--- PrimeiraAula.js
+++ PrimeiraAula.js
@@ -1,3 +1,4 @@
+//Inicio
 let myName = "Rafael";
 let birthCity = "Belo_Horizonte";
 const birthYear = 2001;
@@ -6,6 +7,7 @@ console.log(myName);
 console.log(birthCity);
 console.log(birthYear);
 
+//tipos primitivos
 let patientId = 50;
 let isEnrolled = true;
 const patientInfo = {
@@ -19,6 +21,7 @@ console.log(typeof isEnrolled);
 console.log(typeof patientInfo);
 console.log(typeof patientEmail);
 
+//algumas operações
 const base = 5;
 let height = 8;
 const area = base*height;
@@ -26,3 +29,26 @@ const perimeter = 2*base+2*height
 
 console.log(area);
 console.log(perimeter);
+
+//if e else
+
+const nota = 85;
+if (nota > 80) {
+    console.log("Parabens,você foi aprovada(o)");
+}
+else if (nota < 80 && nota >= 60) {
+    console.log("Você está na nossa lista de espera");
+}
+else {
+    console.log("Você foi reprovada(o)");
+}
+
+//Operador AND
+const comida = 'pão na chapa';
+const bebida = 'cafézinho';
+
+if (bebida === 'cafézinho' && comida === 'pão na chapa') {
+  console.log('Muito obrigado pela refeição :)');
+} else {
+  console.log('Acho que houve um engano com meu pedido');
+}
