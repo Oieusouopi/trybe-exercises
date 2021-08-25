cb0b594842c2c51ec46d3e4504618cd6d10b39f6
OiEuSouOPi <rafasc866@gmail.com>
2021-08-25 16:11:30 -0300

    aprendendo javascript

---
+++ PrimeiraAula.js
@@ -0,0 +1,28 @@
+let myName = "Rafael";
+let birthCity = "Belo_Horizonte";
+const birthYear = 2001;
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+
+let patientId = 50;
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+
+console.log(typeof patientId);
+console.log(typeof isEnrolled);
+console.log(typeof patientInfo);
+console.log(typeof patientEmail);
+
+const base = 5;
+let height = 8;
+const area = base*height;
+const perimeter = 2*base+2*height
+
+console.log(area);
+console.log(perimeter);
