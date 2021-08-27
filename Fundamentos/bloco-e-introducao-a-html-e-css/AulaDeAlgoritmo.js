08c67da999b1f75ba089969dabeab85835275a95
OiEuSouOPi <rafasc866@gmail.com>
2021-08-27 18:20:56 -0300

    aula de algoritmo

---
+++ Fundamentos/bloco-e-introducao-a-html-e-css/AulaDeAlgoritmo.js
@@ -0,0 +1,67 @@
+//Exercicio da aula de array em algoritmo.
+//Exercicio 1
+/*adicionar array.
+adicionar um loop que percorre a array.
+imprimir cada numero que tem na array.
+
+
+//Exercicio 2
+Adicionar uma array
+criar uma váriavel com valor igual a 0.
+adicionar um loop que percorre a array.
+somar cada elemento da array com o valor da soma indo para a variavel
+imprimir o resultado.
+
+//Exercicio 3
+Adicionar uma array
+Criar uma variavel com valor igual a 0
+adcicionar um loop que percorrer a array.
+Somar cada elemento e adciioanr a variavel
+Criar uma variavel que o valor é igual a somar cada elemento dividido pelo numero de elemntos.
+imprimir o resultado da soma pelos elementos
+
+//Exercicio 4
+Adicionar uma array
+criar uma variavel com valor igual a 0
+adicioanr um loop que percorrer a array
+Somar cada elemento e adciioanr a variavel
+Criar uma variavel que o valor é igual a somar cada elemento dividido pelo numero de elemntos.
+Se o resultado da soma dividido pelos elementos for maior que 20
+imprimir "valor maior que 20"
+Se não
+imprimir "Valor menor ou igual a 20".
+
+//Exercicio 5
+Adicionar uma array
+Criar uma variavel com o valor do primiero elemento da array
+adicionar um loop que percorre a array
+Se o número do elemento for maior que o da variavel
+o elemento toma o lugar da váriavel e repete o loop ate encontrar o número maior.
+
+//Exercicio 6
+Adicionar uma array
+Criar uma variável com o valor igual a 0.
+adicionar um loop que percorre a array
+se o resto da array dividido por 2 for diferente de zero
+incrimenta um na variavel
+se o resultado da variavel for maior que zero
+imprimi o resultado
+se nao
+imprimi "nenhum valor impar encontrado".
+
+//Exercicio 7
+Adcionar uma array
+Criar uma variavel com o valor do primeiro elemento da array
+Adicionar um loop que percorre a array
+se numero da array for menor que o da variavel
+variavel atribui o numero da array
+imprimir a variavel
+
+//Exercicio 8
+Criar uma array sem elementos
+Adicionar um loop que incrimenta a array ate 25
+imprimir array
+
+//Exercicio 9
+Adicioanr um loop que percorre a array
+imprimir a variavel dividido por 2
