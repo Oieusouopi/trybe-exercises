16449f05245ada0a5cb2e476c24f9da0ba6c72d3
OiEuSouOPi <rafasc866@gmail.com>
2021-09-27 15:16:00 -0300

    bloco 7.2

---
+++ Fundamentos/JavaScript e ES6/AtiviadeDaAula.js
@@ -0,0 +1,11 @@
+   const objeto = {
+        name:'Rafael',
+        country:'Brazil',
+        job:'Studant',
+    };
+    const age = "age";
+    
+    const value = 20;
+    
+    objeto.age = value;
+    console.log(objeto);
